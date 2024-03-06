641939a7b5da9666a6d05a577e4c46d6f55730e6
sam-kim99 <samkim0451@gmail.com>
2024-03-05 19:34:12 -0500

    finish tic tac toe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-Add your import statements for View and Game here
+import Game from '/home/samkim0451/WEEK 9/W9D2/practice-for-ch-js-browser-ttt-main/ttt_node/game.js';
+import View from './ttt-view';
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const figure = document.getElementsByClassName("ttt")[0];
+  let newGame = new Game ();
+  let newView = new View (newGame, figure);
 });
--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,17 +1,61 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.handleClick = this.handleClick.bind(this);
+    this.setupBoard();
+    this.displayPlayer();
   }
   
+  displayPlayer() {
+    let currentMark = document.getElementById('mark')
+    currentMark.innerText = `Current Player: ${this.game.currentPlayer}`
+  }
+
   setupBoard() {
+    const ul = document.createElement('ul');
+    ul.classList.add('grid')
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        let cell = document.createElement('li');
+        cell.dataset.pos = JSON.stringify([row, col]),
+        ul.appendChild(cell);
+      }
+    }
+    this.el.appendChild(ul);
+    this.el.addEventListener("click", this.handleClick)
   }
   
   handleClick(e) {
+    const move = e.target;
+    "LI" === move.nodeName && this.makeMove(move);
   }
 
   makeMove(square) {
+    const pos = JSON.parse(square.dataset.pos);
+    const symbol = this.game.currentPlayer;
+    try {
+      this.game.playMove(pos)
+      this.displayPlayer();
+    } catch (t) {
+      alert("This " + t.msg.toLowerCase())
+    }
+    square.classList.add(symbol);
+    this.game.isOver() && this.handleGameOver();
   }
   
   handleGameOver() {
+    this.el.removeEventListener('click', this.handleClick);
+    this.el.classList.add('game-over');
+    const winner = this.game.winner();
+    const msg = document.createElement("figcaption")
+    if (winner) {
+      this.el.classList.add(`winner-${winner}`);
+      msg.append(`You win, ${winner}!`);
+    } else {
+      msg.append("It's a draw!");
+    }
+    this.el.append(msg);
   }
 }
 
